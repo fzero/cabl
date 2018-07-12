ed64f1deaef2790a6b229cd55624ba003e804cf2
Fabio Neves <hello+github@fabioneves.ca>
2018-07-12 13:31:19 -0400

    rtmidi includes weren't found automatically

--- clean-build.sh
+++ clean-build.sh
@@ -2,7 +2,7 @@
 rm -rf build
 mkdir build
 cd build
-cmake .. -DBUILD_SHARED_LIBS=ON
+cmake .. -DRTMIDI_INCLUDE_DIRS=/usr/include/rtmidi -DBUILD_SHARED_LIBS=ON
 cd ..
 cmake --build build
 cmake --build build --target check
